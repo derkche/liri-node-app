bdf92edf1d168481109b56136e431bb08dc157f6
derkche <derekchee.40sw@gmail.com>
2018-07-16 17:26:01 -0400

    3 down 1 to go

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
-var request = require("request");
 var inquirer =  require("inquirer");
+var request = require("request");
 
 // Twitter Function
 function getTweets(){
@@ -18,23 +18,21 @@ function getTweets(){
   T.get('search/tweets', params, gotData);
   
   function gotData(err, data, response){
-  
     for (i=0; i<20; i++){
       console.log(JSON.stringify(data.statuses[i].text, null, 2));
     }
-  
   }
 
 };
 
-// Spotify function
-function spotifyThis(query){
+// Spotify Function
+function spotifyThis(songQuery){
 
   var Spotify = require('node-spotify-api');
   var spotconfig = require('./spotconfig');
   var spotify = new Spotify(spotconfig);
   
-  spotify.search({ type: 'track', query: query, limit: 1 }, function(err, data) {
+  spotify.search({ type: 'track', query: songQuery, limit: 1 }, function(err, data) {
     if (err) {
     return console.log('Error occurred: ' + err);
     }
@@ -52,6 +50,23 @@ function spotifyThis(query){
 
 };
 
+// OMDB Function via Request
+function omdbThis(movieQuery){
+    request("http://www.omdbapi.com/?t="+movieQuery+"&y=&plot=short&apikey=trilogy", function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log('Title: '+JSON.parse(body).Title);
+        console.log('Year: '+JSON.parse(body).Year);
+        console.log('IMDB Rating: '+JSON.parse(body).imdbRating);
+        console.log('Rotten Tomatoes Rating: '+JSON.parse(body).Ratings[1].Value); //Rotten Tomatoes
+        console.log('Country: '+JSON.parse(body).Country);
+        console.log('Language: '+JSON.parse(body).Language);
+        console.log('Plot: '+JSON.parse(body).Plot);
+        console.log('Actors: '+JSON.parse(body).Actors);
+      }
+    });
+}
+
+
 inquirer.prompt([
   {
     type: 'list',
@@ -65,13 +80,19 @@ inquirer.prompt([
     ]
   }
 ]).then(function(inquirerResponse) {
+  console.log(inquirerResponse.menu);
 
-  // console.log(inquirerResponse.menu);
-
+  // User chooses Twitter
   if (inquirerResponse.menu == 'My Tweets'){
+
     console.log("Pulling your most recent 20 tweets...");
     getTweets();
-  }else if (inquirerResponse.menu == 'Spotify this song'){
+
+  };
+
+  // User chooses Spotify
+  if (inquirerResponse.menu == 'Spotify this song'){
+
     // ask for a song to query
     inquirer.prompt([
       {
@@ -81,8 +102,24 @@ inquirer.prompt([
       }
     ]).then(function(inquirerResponse){
       // console.log(inquirerResponse.songInput);
+      console.log("Fetching your song info...");
       spotifyThis(inquirerResponse.songInput);
     });
-  }
+
+  };
+
+  // User choses OMDB
+  if(inquirerResponse.menu == 'Movie this'){
+    inquirer.prompt([
+      {
+        type: 'input',
+        message:'Movie name please',
+        name: 'movieInput'
+      }
+    ]).then(function(inquirerResponse){
+      console.log('Going to BlockBuster for '+inquirerResponse.movieInput+'.')
+      omdbThis(inquirerResponse.movieInput);
+    });
+  };
 
 });
